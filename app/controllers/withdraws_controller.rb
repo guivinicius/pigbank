ddb44bee7669fe13e518c9a347bc41641e5118c7
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-02-24 01:09:09 -0300

    Refactoring withdraws

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -1,53 +1,39 @@
 class WithdrawsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :new_withdraw, :except => [:show]
 
   def new
+    @withdraw = Debit.new
   end
 
   def check
-
-    if can_withdraw?
-      @withdraw.amount = params[:debit][:amount]
+    debit_service = DebitService.new(current_user, withdraw_params[:amount])
+    if debit_service.valid?
+      @withdraw = Debit.new(withdraw_params)
     else
-      redirect_to new_withdraw_path, :alert => "You can't withdraw more than your current balance."
+      redirect_to new_withdraw_path, :alert => debit_service.errors.full_messages
     end
 
   end
 
   def create
-    @withdraw.amount = params[:debit][:amount]
-
-    if can_withdraw? && correct_password?
-
-      if @withdraw.save
-        redirect_to withdraw_path(@withdraw), :notice => "Withdrawal completed successfully."
-      else
-        redirect_to new_withdraw_path, :alert => "Sorry. Something happend during the transaction of your withdraw."
-      end
-
+    debit_service = DebitService.new(current_user, withdraw_params[:amount], "Withdraw", params[:password])
+    if debit_service.valid?
+      @withdraw = debit_service.debit!
+      redirect_to withdraw_path(@withdraw), :notice => "Withdrawal completed successfully."
     else
-      redirect_to new_withdraw_path, :alert => "Wrong password."
+      redirect_to new_withdraw_path, :alert => debit_service.errors.full_messages
     end
 
   end
 
   def show
-    @withdraw = current_user.debits.find(params[:id])
+    @withdraw = Debit.find(params[:id])
   end
 
   private
 
-  def new_withdraw
-    @withdraw = current_user.debits.new
-  end
-
-  def can_withdraw?
-    current_user.balance >= BigDecimal(params[:debit][:amount])
-  end
-
-  def correct_password?
-    current_user.valid_password?(params[:password])
+  def withdraw_params
+    params.require(:debit).permit(:amount)
   end
 
 end
---
+++ app/services/debit_service.rb
@@ -0,0 +1,39 @@
+class DebitService
+
+  def initialize(user, amount, description = "Debit", password = "")
+    @user = user
+    @amount = amount
+    @password = password
+
+    @debit = Debit.new(:user => @user, :amount => @amount, :activity_type => 1, :description => description)
+    @debit.valid?
+
+    validate_password if @password.present?
+  end
+
+  def debit!
+
+    ActiveRecord::Base.transaction do
+      @debit.save
+      @debit.user.decrement!(:balance, @debit.amount)
+    end if valid?
+
+    @debit
+  end
+
+  def valid?
+    errors.size == 0
+  end
+
+  def errors
+    @debit.errors
+  end
+
+  private
+
+  def validate_password
+    errors.add(:password, "is wrong.") if !@user.valid_password?(@password)
+  end
+
+
+end
