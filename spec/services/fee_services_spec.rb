2eef68a631b37e8afec83d24c9564d331adc4fc2
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-02-24 08:15:18 -0300

    Fixing FeeService tests

--- spec/services/fee_services_spec.rb
+++ spec/services/fee_services_spec.rb
@@ -4,17 +4,12 @@ describe FeeService do
 
   describe "#calculate" do
 
-    after do
-      Timecop.return
-    end
-
     context "when amount is less than 1000" do
 
       context "and is weekend" do
 
         it "returns 7" do
-          Timecop.freeze(Time.new.end_of_week)
-          expect(FeeService.new(100, Time.zone.now).calculate).to eq(7)
+          expect(FeeService.new(100, Time.new.end_of_week).calculate).to eq(7)
         end
 
       end
@@ -23,36 +18,31 @@ describe FeeService do
 
         context "and is 8am" do
           it "returns 7" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 8))
-            expect(FeeService.new(100, Time.zone.now).calculate).to eq(7)
+            expect(FeeService.new(100, Time.new.beginning_of_week.change(:hour => 8)).calculate).to eq(7)
           end
         end
 
         context "and is 9am" do
           it "returns 5" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 10))
-            expect(FeeService.new(100, Time.zone.now).calculate).to eq(5)
+            expect(FeeService.new(100, Time.new.beginning_of_week.change(:hour => 9)).calculate).to eq(5)
           end
         end
 
         context "and is 12am" do
           it "returns 5" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 12))
-            expect(FeeService.new(100, Time.zone.now).calculate).to eq(5)
+            expect(FeeService.new(100, Time.new.beginning_of_week.change(:hour => 12)).calculate).to eq(5)
           end
         end
 
         context "and is 18pm" do
           it "returns 5" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 18))
-            expect(FeeService.new(100, Time.zone.now).calculate).to eq(5)
+            expect(FeeService.new(100, Time.new.beginning_of_week.change(:hour => 18)).calculate).to eq(5)
           end
         end
 
         context "and is 20pm" do
           it "returns 7" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 20))
-            expect(FeeService.new(100, Time.zone.now).calculate).to eq(7)
+            expect(FeeService.new(100,Time.new.beginning_of_week.change(:hour => 20)).calculate).to eq(7)
           end
         end
 
@@ -64,8 +54,7 @@ describe FeeService do
       context "and is weekend" do
 
         it "returns 17" do
-          Timecop.freeze(Time.new.end_of_week)
-          expect(FeeService.new(10000, Time.zone.now).calculate).to eq(17)
+          expect(FeeService.new(10000, Time.new.end_of_week).calculate).to eq(17)
         end
 
       end
@@ -74,36 +63,31 @@ describe FeeService do
 
         context "and is 8am" do
           it "returns 17" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 8))
-            expect(FeeService.new(10000, Time.zone.now).calculate).to eq(17)
+            expect(FeeService.new(10000, Time.new.beginning_of_week.change(:hour => 8)).calculate).to eq(17)
           end
         end
 
         context "and is 9am" do
           it "returns 15" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 10))
-            expect(FeeService.new(10000, Time.zone.now).calculate).to eq(15)
+            expect(FeeService.new(10000, Time.new.beginning_of_week.change(:hour => 9)).calculate).to eq(15)
           end
         end
 
         context "and is 12am" do
           it "returns 15" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 12))
-            expect(FeeService.new(10000, Time.zone.now).calculate).to eq(15)
+            expect(FeeService.new(10000, Time.new.beginning_of_week.change(:hour => 12)).calculate).to eq(15)
           end
         end
 
         context "and is 18pm" do
           it "returns 15" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 18))
-            expect(FeeService.new(10000, Time.zone.now).calculate).to eq(15)
+            expect(FeeService.new(10000, Time.new.beginning_of_week.change(:hour => 18)).calculate).to eq(15)
           end
         end
 
         context "and is 20pm" do
           it "returns 17" do
-            Timecop.freeze(Time.new.beginning_of_week.change(:hour => 20))
-            expect(FeeService.new(10000, Time.zone.now).calculate).to eq(17)
+            expect(FeeService.new(10000, Time.new.beginning_of_week.change(:hour => 20)).calculate).to eq(17)
           end
         end
 
